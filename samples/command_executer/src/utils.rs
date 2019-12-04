50d897820ed06cb90993d94a292c26374b345ba1
Alexandra Pirvulescu <alexprv@amazon.com>
2019-12-04 13:18:10 +0000

    command-executer: Add command parsing
    Add helper functions that parse the arguments.
    
    Signed-off-by: Alexandra Pirvulescu <alexprv@amazon.com>
    Reviewed-by: Andrei Cipu <acipu@amazon.com>
    Reviewed-by: Petre Eftime <epetre@amazon.com>
    Reviewed-by: Sabin Rapan <sabrapan@amazon.com>

---
+++ samples/command_executer/src/utils.rs
@@ -0,0 +1,63 @@
+use log::error;
+
+pub trait ExitGracefully<T, E> {
+    fn ok_or_exit(self, message: &str) -> T;
+}
+
+impl<T, E: std::fmt::Debug> ExitGracefully<T, E> for Result<T, E> {
+    fn ok_or_exit(self, message: &str) -> T {
+        match self {
+            Ok(val) => val,
+            Err(err) => {
+                error!("{:?}: {}", err, message);
+                std::process::exit(1);
+            }
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! create_app {
+    () => {
+        App::new("Vsock Tool")
+            .about("Tool that runs commands inside the enclave")
+            .setting(AppSettings::ArgRequiredElseHelp)
+            .version(env!("CARGO_PKG_VERSION"))
+            .subcommand(
+                SubCommand::with_name("listen")
+                    .about("Listen on a given port")
+                    .arg(
+                        Arg::with_name("port")
+                            .long("port")
+                            .help("port")
+                            .takes_value(true)
+                            .required(true),
+                    ),
+            )
+            .subcommand(
+                SubCommand::with_name("run")
+                    .about("Run a command inside the enclave")
+                    .arg(
+                        Arg::with_name("port")
+                            .long("port")
+                            .help("port")
+                            .takes_value(true)
+                            .required(true),
+                    )
+                    .arg(
+                        Arg::with_name("cid")
+                            .long("cid")
+                            .help("cid")
+                            .takes_value(true)
+                            .required(true),
+                    )
+                    .arg(
+                        Arg::with_name("command")
+                            .long("command")
+                            .help("command")
+                            .takes_value(true)
+                            .required(true),
+                    ),
+            )
+    };
+}

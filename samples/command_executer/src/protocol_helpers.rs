63f7213f0f4aba423a040954c0aa01504fe6e0ed
Alexandra Pirvulescu <alexprv@amazon.com>
2019-12-04 13:24:42 +0000

    command-executer: Implement the protocol
    Add the functions that implement the actual protocol.
    
    Each message has the following format:
    message_len (8 bytes) + command_no (1 byte) + message
    The command number is not currently used, it will be
    used when more commands will be added.
    
    Signed-off-by: Alexandra Pirvulescu <alexprv@amazon.com>
    Reviewed-by: Andrei Cipu <acipu@amazon.com>
    Reviewed-by: Sabin Rapan <sabrapan@amazon.com>

---
+++ samples/command_executer/src/protocol_helpers.rs
@@ -0,0 +1,52 @@
+use byteorder::{ByteOrder, LittleEndian};
+use nix::errno::Errno::EINTR;
+use nix::sys::socket::MsgFlags;
+use nix::sys::socket::{recv, send};
+use std::convert::TryInto;
+use std::os::unix::io::RawFd;
+
+pub fn send_len(fd: RawFd, len: u64, _cmd: u8) -> Result<(), String> {
+    let mut buf = [0u8; 9];
+    LittleEndian::write_u64(&mut buf, len);
+    send_loop(fd, &mut buf, 9)?;
+    Ok(())
+}
+
+pub fn recv_len(fd: RawFd) -> Result<u64, String> {
+    let mut buf = [0u8; 9];
+    recv_loop(fd, &mut buf, 9)?;
+    let len = LittleEndian::read_u64(&buf);
+    Ok(len)
+}
+
+pub fn send_loop(fd: RawFd, buf: &[u8], len: u64) -> Result<(), String> {
+    let len: usize = len.try_into().map_err(|err| format!("{:?}", err))?;
+    let mut send_bytes = 0;
+
+    while send_bytes < len {
+        let size = match send(fd, &buf[send_bytes..], MsgFlags::empty()) {
+            Ok(size) => size,
+            Err(nix::Error::Sys(EINTR)) => 0,
+            Err(err) => return Err(format!("{:?}", err)),
+        };
+        send_bytes += size;
+    }
+
+    Ok(())
+}
+
+pub fn recv_loop(fd: RawFd, buf: &mut [u8], len: u64) -> Result<(), String> {
+    let len: usize = len.try_into().map_err(|err| format!("{:?}", err))?;
+    let mut recv_bytes = 0;
+
+    while recv_bytes < len {
+        let size = match recv(fd, &mut buf[recv_bytes..], MsgFlags::empty()) {
+            Ok(size) => size,
+            Err(nix::Error::Sys(EINTR)) => 0,
+            Err(err) => return Err(format!("{:?}", err)),
+        };
+        recv_bytes += size;
+    }
+
+    Ok(())
+}

a1e6a5ca4cd6c9d66a977b55fb0c2857c9b942f1
Cristian Done <cdone@amazon.com>
2020-01-23 17:11:51 +0200

    samples/command_executer: Add send-file
    This commit aims to improve the functionalities of the command-executer
    by adding send-file command to send a file to server.
    
    Tested using the following commands:
    enclave$ /command-executer listen --port 10000
    // Send file tests and save it as /rtests
    guest$ command-executer send-file --cid 16 --port 10000 --localpath tests --remotepath /rtests
    
    Signed-off-by: Cristian Done <cdone@amazon.com>
    Reviewed-by: Alexandru-Cosmin Gheorghe <aggh@amazon.com>
    Reviewed-by: Alexandra Pirvulescu <alexprv@amazon.com>
    Reviewed-by: Alexandru-Catalin Vasile <lexnv@amazon.com>

--- samples/command_executer/src/main.rs
+++ samples/command_executer/src/main.rs
@@ -1,9 +1,9 @@
 use clap::{App, AppSettings, Arg, SubCommand};
 
-use command_executer::command_parser::{ListenArgs, RecvFileArgs, RunArgs};
+use command_executer::command_parser::{FileArgs, ListenArgs, RunArgs};
 use command_executer::create_app;
 use command_executer::utils::ExitGracefully;
-use command_executer::{listen, recv_file, run};
+use command_executer::{listen, recv_file, run, send_file};
 
 fn main() {
     let app = create_app!();
@@ -19,8 +19,12 @@ fn main() {
             run(run_args).ok_or_exit(args.usage());
         }
         ("recv-file", Some(args)) => {
-            let subcmd_args = RecvFileArgs::new_with(args).ok_or_exit(args.usage());
+            let subcmd_args = FileArgs::new_with(args).ok_or_exit(args.usage());
             recv_file(subcmd_args).ok_or_exit(args.usage());
+        }
+        ("send-file", Some(args)) => {
+            let subcmd_args = FileArgs::new_with(args).ok_or_exit(args.usage());
+            send_file(subcmd_args).ok_or_exit(args.usage());
         }
         (&_, _) => {}
     }

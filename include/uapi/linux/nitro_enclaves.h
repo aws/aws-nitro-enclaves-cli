79a865047fc3b79a01f91b9ce2d6352cecb448d4
Andrei Trandafir <aatrand@amazon.com>
2020-08-12 12:40:27 +0300

    cli: Update driver to version v6
    Signed-off-by: Andrei Trandafir <aatrand@amazon.com>

--- include/uapi/linux/nitro_enclaves.h
+++ include/uapi/linux/nitro_enclaves.h
@@ -8,201 +8,320 @@
 
 #include <linux/types.h>
 
-/* Nitro Enclaves (NE) Kernel Driver Interface */
-
-#define NE_API_VERSION (1)
-
 /**
- * The command is used to get the version of the NE API. This way the user space
- * processes can be aware of the feature sets provided by the NE kernel driver.
- *
- * The NE API version is returned as result of this ioctl call.
- *
- * The ioctl can be invoked on the /dev/nitro_enclaves fd, independent of
- * enclaves already created / started or not.
- *
- * No errors are returned.
+ * DOC: Nitro Enclaves (NE) Kernel Driver Interface
  */
-#define NE_GET_API_VERSION _IO(0xAE, 0x20)
 
 /**
- * The command is used to create a slot that is associated with an enclave VM.
- * Memory and vCPUs are then set for the slot mapped to an enclave.
+ * NE_CREATE_VM - The command is used to create a slot that is associated with
+ *		  an enclave VM.
+ *		  The generated unique slot id is an output parameter.
+ *		  The ioctl can be invoked on the /dev/nitro_enclaves fd, before
+ *		  setting any resources, such as memory and vCPUs, for an
+ *		  enclave. Memory and vCPUs are set for the slot mapped to an enclave.
+ *		  A NE CPU pool has to be set before calling this function. The
+ *		  pool can be set after the NE driver load, using
+ *		  /sys/module/nitro_enclaves/parameters/ne_cpus.
+ *		  Its format is the detailed in the cpu-lists section:
+ *		  https://www.kernel.org/doc/html/latest/admin-guide/kernel-parameters.html
+ *		  CPU 0 and its siblings have to remain available for the
+ *		  primary / parent VM, so they cannot be set for enclaves. Full
+ *		  CPU core(s), from the same NUMA node, need(s) to be included
+ *		  in the CPU pool.
  *
- * The generated unique slot id is an output parameter. An enclave file
- * descriptor is returned as result of this ioctl call. The enclave fd can be
- * further used with ioctl calls to set vCPUs and memory regions, then start
- * the enclave.
- *
- * The ioctl can be invoked on the /dev/nitro_enclaves fd, before setting any
- * resources, such as memory and vCPUs, for an enclave.
- *
- * A NE CPU pool has be set before calling this function. The pool can be set
- * after the NE driver load, using /sys/module/nitro_enclaves/parameters/ne_cpus.
- * Its format is the following:
- * https://www.kernel.org/doc/html/latest/admin-guide/kernel-parameters.html#cpu-lists
- *
- * CPU 0 and its siblings have to remain available for the primary / parent VM,
- * so they cannot be set for enclaves. Full CPU core(s), from the same NUMA
- * node, need(s) be included in the CPU pool.
- *
- * The returned errors are:
- * * -EFAULT - copy_to_user() failure.
- * * -ENOMEM - memory allocation failure for internal bookkeeping variables.
- * * -EINVAL - no internal data structure available for the NE PCI device.
- *           - no NE CPU pool set.
+ * Context: Process context.
+ * Return:
+ * * Enclave file descriptor		- Enclave file descriptor used with
+ *					  ioctl calls to set vCPUs and memory
+ *					  regions, then start the enclave.
+ * * -EFAULT				- copy_to_user() failure.
+ * * -ENOMEM				- Memory allocation failure for internal
+ *					  bookkeeping variables.
+ * * -NE_ERR_NO_CPUS_AVAIL_IN_POOL	- No NE CPU pool set / no CPUs available
+ *					  in the pool.
  * * Error codes from get_unused_fd_flags() and anon_inode_getfile().
  * * Error codes from the NE PCI device request.
  */
-#define NE_CREATE_VM _IOR(0xAE, 0x21, __u64)
+#define NE_CREATE_VM			_IOR(0xAE, 0x20, __u64)
 
 /**
- * The command is used to set a vCPU for an enclave. The vCPU can be auto-chosen
- * from the NE CPU pool or it can be set by the caller, with the note that it
- * needs to be available in the NE CPU pool. Full CPU core(s), from the same
- * NUMA node, need(s) to be associated with an enclave.
+ * NE_ADD_VCPU - The command is used to set a vCPU for an enclave. The vCPU can
+ *		 be auto-chosen from the NE CPU pool or it can be set by the
+ *		 caller, with the note that it needs to be available in the NE
+ *		 CPU pool. Full CPU core(s), from the same NUMA node, need(s) to
+ *		 be associated with an enclave.
+ *		 The vCPU id is an input / output parameter. If its value is 0,
+ *		 then a CPU is chosen from the enclave CPU pool and returned via
+ *		 this parameter.
+ *		 The ioctl can be invoked on the enclave fd, before an enclave
+ *		 is started.
  *
- * The vCPU id is an input / output parameter. If its value is 0, then a CPU is
- * chosen from the enclave CPU pool and returned via this parameter. A vCPU file
- * descriptor is returned as result of this ioctl call.
- *
- * The ioctl can be invoked on the enclave fd, before an enclave is started.
- *
- * The returned errors are:
- * * -EFAULT - copy_from_user() / copy_to_user() failure.
- * * -ENOMEM - memory allocation failure for internal bookkeeping variables.
- * * -EINVAL - no CPUs available in the NE CPU pool.
- *           - the vCPU that is set is not available in the NE CPU pool.
- *           - the enclave is not in init state (init = before being started).
- * * -EIO - current task mm is not the same as the one that created the enclave.
- * * Error codes from get_unused_fd_flags() and anon_inode_getfile().
+ * Context: Process context.
+ * Return:
+ * * 0					- Logic succesfully completed.
+ * * -EFAULT				- copy_from_user() / copy_to_user() failure.
+ * * -ENOMEM				- Memory allocation failure for internal
+ *					  bookkeeping variables.
+ * * -EIO				- Current task mm is not the same as the one
+ *					  that created the enclave.
+ * * -NE_ERR_NO_CPUS_AVAIL_IN_POOL	- No CPUs available in the NE CPU pool.
+ * * -NE_ERR_VCPU_ALREADY_USED		- The provided vCPU is already used.
+ * * -NE_ERR_VCPU_NOT_IN_CPU_POOL	- The provided vCPU is not available in the
+ *					  NE CPU pool.
+ * * -NE_ERR_VCPU_INVALID_CPU_CORE	- The core id of the provided vCPU is invalid
+ *					  or out of range.
+ * * -NE_ERR_NOT_IN_INIT_STATE		- The enclave is not in init state
+ *					  (init = before being started).
+ * * -NE_ERR_INVALID_VCPU		- The provided vCPU is not in the available
+ *					  CPUs range.
  * * Error codes from the NE PCI device request.
  */
-#define NE_CREATE_VCPU _IOWR(0xAE, 0x22, __u32)
+#define NE_ADD_VCPU			_IOWR(0xAE, 0x21, __u32)
 
 /**
- * The command is used to get information needed for in-memory enclave image
- * loading e.g. offset in enclave memory to start placing the enclave image.
+ * NE_GET_IMAGE_LOAD_INFO - The command is used to get information needed for
+ *			    in-memory enclave image loading e.g. offset in
+ *			    enclave memory to start placing the enclave image.
+ *			    The image load info is an input / output parameter.
+ *			    It includes info provided by the caller - flags -
+ *			    and returns the offset in enclave memory where to
+ *			    start placing the enclave image.
+ *			    The ioctl can be invoked on the enclave fd, before
+ *			    an enclave is started.
  *
- * The image load info is an input / output parameter. It includes info provided
- * by the caller - flags - and returns the offset in enclave memory where to
- * start placing the enclave image.
- *
- * The ioctl can be invoked on the enclave fd, before an enclave is started.
- *
- * The returned errors are:
- * * -EFAULT - copy_from_user() / copy_to_user() failure.
- * * -EINVAL - the enclave is not in init state (init = before being started).
+ * Context: Process context.
+ * Return:
+ * * 0				- Logic succesfully completed.
+ * * -EFAULT			- copy_from_user() / copy_to_user() failure.
+ * * -EINVAL			- Invalid flag value.
+ * * -NE_ERR_NOT_IN_INIT_STATE	- The enclave is not in init state (init =
+ *				  before being started).
  */
-#define NE_GET_IMAGE_LOAD_INFO _IOWR(0xAE, 0x23, struct ne_image_load_info)
+#define NE_GET_IMAGE_LOAD_INFO		_IOWR(0xAE, 0x22, struct ne_image_load_info)
 
 /**
- * The command is used to set a memory region for an enclave, given the
- * allocated memory from the userspace. Enclave memory needs to be from the
- * same NUMA node as the enclave CPUs.
+ * NE_SET_USER_MEMORY_REGION - The command is used to set a memory region for an
+ *			       enclave, given the allocated memory from the
+ *			       userspace. Enclave memory needs to be from the
+ *			       same NUMA node as the enclave CPUs.
+ *			       The user memory region is an input parameter. It
+ *			       includes info provided by the caller - flags,
+ *			       memory size and userspace address.
+ *			       The ioctl can be invoked on the enclave fd,
+ *			       before an enclave is started.
  *
- * The user memory region is an input parameter. It includes info provided
- * by the caller - flags, memory size and userspace address.
- *
- * The ioctl can be invoked on the enclave fd, before an enclave is started.
- *
- * The returned errors are:
- * * -EFAULT - copy_from_user() failure.
- * * -ENOMEM - memory allocation failure for internal bookkeeping variables.
- * * -EINVAL - the enclave is not in init state (init = before being started).
- *           - the memory size of the region is not multiple of 2 MiB.
- *           - invalid user space address given.
- *           - the memory region is not from the same NUMA node as the CPUs.
- *           - the number of memory regions set for the enclave reached maximum.
- *           - the physical memory region are not minimum 2 MiB and aligned.
- * * -EIO - current task mm is not the same as the one that created the enclave.
+ * Context: Process context.
+ * Return:
+ * * 0					- Logic succesfully completed.
+ * * -EFAULT				- copy_from_user() failure.
+ * * -EINVAL				- Invalid flag value.
+ * * -EIO				- Current task mm is not the same as
+ *					  the one that created the enclave.
+ * * -ENOMEM				- Memory allocation failure for internal
+ *					  bookkeeping variables.
+ * * -NE_ERR_NOT_IN_INIT_STATE		- The enclave is not in init state
+ *					  (init = before being started).
+ * * -NE_ERR_INVALID_MEM_REGION_SIZE	- The memory size of the region is not
+ *					  multiple of 2 MiB.
+ * * -NE_ERR_INVALID_MEM_REGION_ADDR	- Invalid user space address given.
+ * * -NE_ERR_UNALIGNED_MEM_REGION_ADDR	- Unaligned user space address given.
+ * * -NE_ERR_MEM_REGION_ALREADY_USED	- The memory region is already used.
+ * * -NE_ERR_MEM_NOT_HUGE_PAGE		- The memory regions is not backed by
+ *					  huge pages.
+ * * -NE_ERR_MEM_DIFFERENT_NUMA_NODE	- The memory region is not from the same
+ *					  NUMA node as the CPUs.
+ * * -NE_ERR_MEM_MAX_REGIONS		- The number of memory regions set for
+ *					  the enclave reached maximum.
  * * Error codes from get_user_pages().
  * * Error codes from the NE PCI device request.
  */
-#define NE_SET_USER_MEMORY_REGION _IOW(0xAE, 0x24, struct ne_user_memory_region)
+#define NE_SET_USER_MEMORY_REGION	_IOW(0xAE, 0x23, struct ne_user_memory_region)
 
 /**
- * The command is used to trigger enclave start after the enclave resources,
- * such as memory and CPU, have been set.
+ * NE_START_ENCLAVE - The command is used to trigger enclave start after the
+ *		      enclave resources, such as memory and CPU, have been set.
+ *		      The enclave start info is an input / output parameter. It
+ *		      includes info provided by the caller - enclave cid and
+ *		      flags - and returns the cid (if input cid is 0).
+ *		      The ioctl can be invoked on the enclave fd, after an
+ *		      enclave slot is created and resources, such as memory and
+ *		      vCPUs are set for an enclave.
  *
- * The enclave start info is an input / output parameter. It includes info
- * provided by the caller - enclave cid and flags - and returns the cid (if
- * input cid is 0).
- *
- * The ioctl can be invoked on the enclave fd, after an enclave slot is created
- * and resources, such as memory and vCPUs are set for an enclave.
- *
- * The returned errors are:
- * * -EFAULT - copy_from_user() / copy_to_user() failure.
- * * -EINVAL - the enclave is not in init state (init = before being started).
- *           - no CPUs / memory regions are set for the enclave.
- *           - full core(s) not set for the enclave.
- *           - enclave memory is less than minimum memory size (64 MiB).
+ * Context: Process context.
+ * Return:
+ * * 0					- Logic succesfully completed.
+ * * -EFAULT				- copy_from_user() / copy_to_user() failure.
+ * * -EINVAL				- Invalid flag value.
+ * * -NE_ERR_NOT_IN_INIT_STATE		- The enclave is not in init state
+ *					  (init = before being started).
+ * * -NE_ERR_NO_MEM_REGIONS_ADDED	- No memory regions are set.
+ * * -NE_ERR_NO_VCPUS_ADDED		- No vCPUs are set.
+ * * -NE_ERR_FULL_CORES_NOT_USED	- Full core(s) not set for the enclave.
+ * * -NE_ERR_ENCLAVE_MEM_MIN_SIZE	- Enclave memory is less than minimum
+ *					  memory size (64 MiB).
  * * Error codes from the NE PCI device request.
  */
-#define NE_START_ENCLAVE _IOWR(0xAE, 0x25, struct ne_enclave_start_info)
+#define NE_START_ENCLAVE		_IOWR(0xAE, 0x24, struct ne_enclave_start_info)
 
-/* Image load info flags */
+/**
+ * DOC: NE specific error codes
+ */
 
-/* Enclave Image Format (EIF) */
-#define NE_EIF_IMAGE (0x01)
+/**
+ * NE_ERR_VCPU_ALREADY_USED - The provided vCPU is already used.
+ */
+#define NE_ERR_VCPU_ALREADY_USED		(512)
+/**
+ * NE_ERR_VCPU_NOT_IN_CPU_POOL - The provided vCPU is not available in the
+ *				 NE CPU pool.
+ */
+#define NE_ERR_VCPU_NOT_IN_CPU_POOL		(513)
+/**
+ * NE_ERR_VCPU_INVALID_CPU_CORE - The core id of the provided vCPU is invalid
+ *				  or out of range of the NE CPU pool.
+ */
+#define NE_ERR_VCPU_INVALID_CPU_CORE		(514)
+/**
+ * NE_ERR_INVALID_MEM_REGION_SIZE - The user space memory region size is not
+ *				    multiple of 2 MiB.
+ */
+#define NE_ERR_INVALID_MEM_REGION_SIZE		(515)
+/**
+ * NE_ERR_INVALID_MEM_REGION_ADDR - The user space memory region address range
+ *				    is invalid.
+ */
+#define NE_ERR_INVALID_MEM_REGION_ADDR		(516)
+/**
+ * NE_ERR_UNALIGNED_MEM_REGION_ADDR - The user space memory region address is
+ *				      not aligned.
+ */
+#define NE_ERR_UNALIGNED_MEM_REGION_ADDR	(517)
+/**
+ * NE_ERR_MEM_REGION_ALREADY_USED - The user space memory region is already used.
+ */
+#define NE_ERR_MEM_REGION_ALREADY_USED		(518)
+/**
+ * NE_ERR_MEM_NOT_HUGE_PAGE - The user space memory region is not backed by
+ *			      contiguous physical huge page(s).
+ */
+#define NE_ERR_MEM_NOT_HUGE_PAGE		(519)
+/**
+ * NE_ERR_MEM_DIFFERENT_NUMA_NODE - The user space memory region is backed by
+ *				    pages from different NUMA nodes than the CPUs.
+ */
+#define NE_ERR_MEM_DIFFERENT_NUMA_NODE		(520)
+/**
+ * NE_ERR_MEM_MAX_REGIONS - The supported max memory regions per enclaves has
+ *			    been reached.
+ */
+#define NE_ERR_MEM_MAX_REGIONS			(521)
+/**
+ * NE_ERR_NO_MEM_REGIONS_ADDED - The command to start an enclave is triggered
+ *				 and no memory regions are added.
+ */
+#define NE_ERR_NO_MEM_REGIONS_ADDED		(522)
+/**
+ * NE_ERR_NO_VCPUS_ADDED - The command to start an enclave is triggered and no
+ *			   vCPUs are added.
+ */
+#define NE_ERR_NO_VCPUS_ADDED			(523)
+/**
+ * NE_ERR_ENCLAVE_MEM_MIN_SIZE - The enclave memory size is lower than the
+ *				 minimum supported.
+ */
+#define NE_ERR_ENCLAVE_MEM_MIN_SIZE		(524)
+/**
+ * NE_ERR_FULL_CORES_NOT_USED - The command to start an enclave is triggered and
+ *				full CPU cores are not set.
+ */
+#define NE_ERR_FULL_CORES_NOT_USED		(525)
+/**
+ * NE_ERR_NOT_IN_INIT_STATE - The enclave is not in init state when setting
+ *			      resources or triggering start.
+ */
+#define NE_ERR_NOT_IN_INIT_STATE		(526)
+/**
+ * NE_ERR_INVALID_VCPU - The provided vCPU is out of range of the available CPUs.
+ */
+#define NE_ERR_INVALID_VCPU			(527)
+/**
+ * NE_ERR_NO_CPUS_AVAIL_IN_POOL - The command to create an enclave is triggered
+ *				  and no CPUs are available in the pool.
+ */
+#define NE_ERR_NO_CPUS_AVAIL_IN_POOL		(528)
 
-/* Info necessary for in-memory enclave image loading (in / out). */
+/**
+ * DOC: Image load info flags
+ */
+
+/**
+ * NE_EIF_IMAGE - Enclave Image Format (EIF)
+ */
+#define NE_EIF_IMAGE	(0x01)
+
+/**
+ * struct ne_image_load_info - Info necessary for in-memory enclave image
+ *			       loading (in / out).
+ * @flags:		Flags to determine the enclave image type
+ *			(e.g. Enclave Image Format - EIF) (in).
+ * @memory_offset:	Offset in enclave memory where to start placing the
+ *			enclave image (out).
+ */
 struct ne_image_load_info {
-	/**
-	 * Flags to determine the enclave image type (e.g. Enclave Image Format
-	 * - EIF) (in).
-	 */
-	__u64 flags;
-
-	/**
-	 * Offset in enclave memory where to start placing the enclave image
-	 * (out).
-	 */
-	__u64 memory_offset;
+	__u64	flags;
+	__u64	memory_offset;
 };
 
-/* User memory region flags */
+/**
+ * DOC: User memory region flags
+ */
 
-/* Memory region for enclave general usage. */
-#define NE_DEFAULT_MEMORY_REGION (0x00)
+/**
+ * NE_DEFAULT_MEMORY_REGION - Memory region for enclave general usage.
+ */
+#define NE_DEFAULT_MEMORY_REGION	(0x00)
 
-/* Memory region to be set for an enclave (in). */
+#define NE_MEMORY_REGION_MAX_FLAG_VAL	(0x01)
+
+/**
+ * struct ne_user_memory_region - Memory region to be set for an enclave (in).
+ * @flags:		Flags to determine the usage for the memory region (in).
+ * @memory_size:	The size, in bytes, of the memory region to be set for
+ *			an enclave (in).
+ * @userspace_addr:	The start address of the userspace allocated memory of
+ *			the memory region to set for an enclave (in).
+ */
 struct ne_user_memory_region {
-	/**
-	 * Flags to determine the usage for the memory region (in).
-	 */
-	__u64 flags;
-
-	/**
-	 * The size, in bytes, of the memory region to be set for an enclave
-	 * (in).
-	 */
-	__u64 memory_size;
-
-	/**
-	 * The start of the userspace allocated memory of the memory region to
-	 * set for an enclave (in).
-	 */
-	__u64 userspace_addr;
+	__u64	flags;
+	__u64	memory_size;
+	__u64	userspace_addr;
 };
 
-/* Enclave start info flags */
+/**
+ * DOC: Enclave start info flags
+ */
 
-/* Start enclave in debug mode. */
-#define NE_ENCLAVE_DEBUG_MODE (0x01)
+/**
+ * NE_ENCLAVE_PRODUCTION_MODE - Start enclave in production mode.
+ */
+#define NE_ENCLAVE_PRODUCTION_MODE	(0x00)
+/**
+ * NE_ENCLAVE_DEBUG_MODE - Start enclave in debug mode.
+ */
+#define NE_ENCLAVE_DEBUG_MODE		(0x01)
 
-/* Setup info necessary for enclave start (in / out). */
+#define NE_ENCLAVE_START_MAX_FLAG_VAL	(0x02)
+
+/**
+ * struct ne_enclave_start_info - Setup info necessary for enclave start (in / out).
+ * @flags:		Flags for the enclave to start with (e.g. debug mode) (in).
+ * @enclave_cid:	Context ID (CID) for the enclave vsock device. If 0 as
+ *			input, the CID is autogenerated by the hypervisor and
+ *			returned back as output by the driver (in / out).
+ */
 struct ne_enclave_start_info {
-	/* Flags for the enclave to start with (e.g. debug mode) (in). */
-	__u64 flags;
-
-	/**
-	 * Context ID (CID) for the enclave vsock device. If 0 as input, the
-	 * CID is autogenerated by the hypervisor and returned back as output
-	 * by the driver (in / out).
-	 */
-	__u64 enclave_cid;
+	__u64	flags;
+	__u64	enclave_cid;
 };
 
 #endif /* _UAPI_LINUX_NITRO_ENCLAVES_H_ */

24b619cd6f08c6fbca44ffb5195c51c2c0548075
Gabriel Bercaru <bercarug@amazon.com>
2020-09-10 14:28:25 +0300

    cli: Refactor error handling for enclave process commands
    Signed-off-by: Gabriel Bercaru <bercarug@amazon.com>

--- src/enclave_proc/commands.rs
+++ src/enclave_proc/commands.rs
@@ -7,13 +7,15 @@ use log::debug;
 use std::fs::File;
 
 use crate::common::commands_parser::RunEnclavesArgs;
+use crate::common::construct_error_message;
 use crate::common::json_output::EnclaveTerminateInfo;
-use crate::common::NitroCliResult;
+use crate::common::{NitroCliErrorEnum, NitroCliFailure, NitroCliResult};
 use crate::enclave_proc::connection::Connection;
 use crate::enclave_proc::connection::{safe_conn_eprintln, safe_conn_println};
 use crate::enclave_proc::cpu_info::CpuInfo;
 use crate::enclave_proc::resource_manager::{EnclaveManager, EnclaveState};
 use crate::enclave_proc::utils::get_enclave_describe_info;
+use crate::new_nitro_cli_failure;
 
 /// Launch an enclave with the specified arguments and provide the launch status through the given connection.
 pub fn run_enclaves(
@@ -22,10 +24,17 @@ pub fn run_enclaves(
 ) -> NitroCliResult<EnclaveManager> {
     debug!("run_enclaves");
 
-    let eif_file =
-        File::open(&args.eif_path).map_err(|e| format!("Failed to open the eif file: {}", e))?;
+    let eif_file = File::open(&args.eif_path).map_err(|e| {
+        new_nitro_cli_failure!(
+            &format!("Failed to open the EIF file: {:?}", e),
+            NitroCliErrorEnum::FileOperationFailure
+        )
+    })?;
 
-    let cpu_ids = CpuInfo::new()?.get_cpu_config(args)?;
+    let cpu_ids = CpuInfo::new()
+        .map_err(|e| e.add_subaction("Failed to construct CPU information".to_string()))?
+        .get_cpu_config(args)
+        .map_err(|e| e.add_subaction("Failed to get CPU configuration".to_string()))?;
     let mut enclave_manager = EnclaveManager::new(
         args.enclave_cid,
         args.memory_mib,
@@ -33,13 +42,15 @@ pub fn run_enclaves(
         eif_file,
         args.debug_mode.unwrap_or(false),
     )
-    .map_err(|e| format!("Failed to create enclave: {}", e))?;
+    .map_err(|e| {
+        e.add_subaction("Failed to construct EnclaveManager with given arguments".to_string())
+    })?;
     enclave_manager
         .run_enclave(connection)
-        .map_err(|e| format!("Failed to run enclave: {}", e))?;
+        .map_err(|e| e.add_subaction("Failed to run enclave".to_string()))?;
     enclave_manager
         .update_state(EnclaveState::Running)
-        .map_err(|e| format!("Failed to update state: {}", e))?;
+        .map_err(|e| e.add_subaction("Failed to update enclave state".to_string()))?;
 
     Ok(enclave_manager)
 }
@@ -52,17 +63,20 @@ pub fn terminate_enclaves(
     let enclave_id = enclave_manager.enclave_id.clone();
 
     debug!("terminate_enclaves");
-    enclave_manager.update_state(EnclaveState::Terminating)?;
-    if let Err(err) = enclave_manager.terminate_enclave() {
+    enclave_manager
+        .update_state(EnclaveState::Terminating)
+        .map_err(|e| e.add_subaction("Failed to update enclave state".to_string()))?;
+    if let Err(error_info) = enclave_manager.terminate_enclave() {
         safe_conn_eprintln(
             connection,
             format!(
                 "Warning: Failed to stop enclave {}\nError message: {:?}",
-                enclave_manager.enclave_id, err
+                enclave_manager.enclave_id,
+                construct_error_message(&error_info).as_str()
             )
             .as_str(),
         )?;
-        return Err(err);
+        return Err(error_info);
     }
 
     enclave_manager.update_state(EnclaveState::Empty)?;
@@ -79,7 +93,12 @@ pub fn terminate_enclaves(
     safe_conn_println(
         connection,
         serde_json::to_string_pretty(&EnclaveTerminateInfo::new(enclave_id, true))
-            .map_err(|err| format!("{:?}", err))?
+            .map_err(|err| {
+                new_nitro_cli_failure!(
+                    &format!("Failed to display enclave termination data: {:?}", err),
+                    NitroCliErrorEnum::SerdeError
+                )
+            })?
             .as_str(),
     )
 }
@@ -91,10 +110,16 @@ pub fn describe_enclaves(
 ) -> NitroCliResult<()> {
     debug!("describe_enclaves");
 
-    let info = get_enclave_describe_info(enclave_manager)?;
+    let info = get_enclave_describe_info(enclave_manager)
+        .map_err(|e| e.add_subaction(String::from("Execute Describe Enclave command")))?;
     connection.println(
         serde_json::to_string_pretty(&info)
-            .map_err(|err| format!("{:?}", err))?
+            .map_err(|err| {
+                new_nitro_cli_failure!(
+                    &format!("Failed to display enclave describe data: {:?}", err),
+                    NitroCliErrorEnum::SerdeError
+                )
+            })?
             .as_str(),
     )
 }

ed9600a62cad1b08ae7110ad9b4b22e6166a1ff0
Gabriel Bercaru <bercarug@amazon.com>
2020-09-10 14:24:42 +0300

    cli: Refactor error handling for logging
    This commit changes the way logging behaves, in the sense
    that logger errors no longer cause sudden exits, but the
    error is propagated to the top module, in order to construct
    the error backtrace.
    
    Signed-off-by: Gabriel Bercaru <bercarug@amazon.com>

--- src/common/logger.rs
+++ src/common/logger.rs
@@ -10,13 +10,14 @@ use flexi_logger::{DeferredNow, LogTarget, Record};
 use nix::unistd::Uid;
 use std::env;
 use std::fs::{File, OpenOptions, Permissions};
-use std::io::{Result, Write};
+use std::io::{Error, ErrorKind, Result, Write};
 use std::ops::{Deref, DerefMut};
 use std::os::unix::fs::{MetadataExt, PermissionsExt};
 use std::path::{Path, PathBuf};
 use std::sync::{Arc, Mutex};
 
-use crate::common::ExitGracefully;
+use crate::common::{NitroCliErrorEnum, NitroCliFailure, NitroCliResult};
+use crate::new_nitro_cli_failure;
 
 /// The default logging level used by the logger.
 const DEFAULT_LOG_LEVEL: &str = "info";
@@ -42,44 +43,61 @@ pub struct EnclaveProcLogWriter {
 
 impl EnclaveProcLogWriter {
     /// Create a new log writer.
-    pub fn new() -> Result<Self> {
+    pub fn new() -> NitroCliResult<Self> {
         // All logging shall be directed to a centralized file.
         Ok(EnclaveProcLogWriter {
-            out_file: Arc::new(Mutex::new(open_log_file(&get_log_file_path())?)),
+            out_file: Arc::new(Mutex::new(
+                open_log_file(&get_log_file_path())
+                    .map_err(|e| e.add_subaction("Failed to open log file".to_string()))?,
+            )),
             logger_id: Arc::new(Mutex::new(String::new())),
         })
     }
 
     /// Check if the log file is present and if it is not, (re)open it.
-    fn safe_open_log_file(&self) {
+    fn safe_open_log_file(&self) -> NitroCliResult<()> {
         let log_path = &get_log_file_path();
         if !log_path.exists() {
-            let new_file = open_log_file(log_path).ok_or_exit("Failed to open log file.");
-            let mut file_ref = self.out_file.lock().ok_or_exit("Failed to lock log file.");
+            let new_file = open_log_file(log_path)
+                .map_err(|e| e.add_subaction(String::from("Failed to open log file")))?;
+            let mut file_ref = self.out_file.lock().map_err(|e| {
+                new_nitro_cli_failure!(
+                    &format!("Failed to acquire lock: {:?}", e),
+                    NitroCliErrorEnum::LockAcquireFailure
+                )
+            })?;
             *file_ref.deref_mut() = new_file;
         }
+
+        Ok(())
     }
 
     /// Update the logger ID (correlated with the process which is doing logging).
-    pub fn update_logger_id(&self, new_id: &str) {
-        let mut old_id = self
-            .logger_id
-            .lock()
-            .ok_or_exit("Failed to lock logger ID.");
+    pub fn update_logger_id(&self, new_id: &str) -> NitroCliResult<()> {
+        let mut old_id = self.logger_id.lock().map_err(|e| {
+            new_nitro_cli_failure!(
+                &format!("Failed to acquire logger ID lock: {:?}", e),
+                NitroCliErrorEnum::LockAcquireFailure
+            )
+        })?;
         old_id.deref_mut().clear();
         old_id.deref_mut().push_str(new_id);
+
+        Ok(())
     }
 
     /// Generate a single message string.
-    fn create_msg(&self, now: &DateTime<Local>, record: &Record) -> String {
+    fn create_msg(&self, now: &DateTime<Local>, record: &Record) -> NitroCliResult<String> {
         // UTC timestamp according to RFC 2822
         let timestamp = DateTime::<Utc>::from_utc(now.naive_utc(), Utc)
             .to_rfc3339_opts(chrono::SecondsFormat::Millis, true);
-        let logger_id = self
-            .logger_id
-            .lock()
-            .ok_or_exit("Failed to lock logger ID.");
-        format!(
+        let logger_id = self.logger_id.lock().map_err(|e| {
+            new_nitro_cli_failure!(
+                &format!("Failed to acquire logger ID lock: {:?}", e),
+                NitroCliErrorEnum::LockAcquireFailure
+            )
+        })?;
+        Ok(format!(
             "[{}][{}][{}][{}:{}] {}\n",
             logger_id.deref(),
             record.level(),
@@ -87,17 +105,33 @@ impl EnclaveProcLogWriter {
             record.file().unwrap_or("?"),
             record.line().unwrap_or(0),
             &record.args()
-        )
+        ))
     }
 }
 
 impl LogWriter for EnclaveProcLogWriter {
     fn write(&self, now: &mut DeferredNow, record: &Record) -> Result<()> {
-        self.safe_open_log_file();
-        let record_str = self.create_msg(now.now(), record);
-        let mut out_file = self.out_file.lock().ok_or_exit("Failed to lock log file.");
-        out_file.deref_mut().write_all(&record_str.as_bytes())?;
-        Ok(())
+        if self.safe_open_log_file().is_err() {
+            return Err(Error::new(
+                ErrorKind::Other,
+                "Failed to safely open log file for writing",
+            ));
+        }
+
+        if let Ok(record_str) = self.create_msg(now.now(), record) {
+            if let Ok(mut out_file) = self.out_file.lock() {
+                out_file.deref_mut().write_all(&record_str.as_bytes())?;
+
+                return Ok(());
+            }
+
+            return Err(Error::new(ErrorKind::Other, "Failed to lock log file"));
+        }
+
+        Err(Error::new(
+            ErrorKind::Other,
+            "Failed to create logger message",
+        ))
     }
 
     fn flush(&self) -> Result<()> {
@@ -120,48 +154,78 @@ impl LogWriter for EnclaveProcLogWriter {
     }
 }
 
-/// Get the path to the log file.
-fn get_log_file_path() -> PathBuf {
-    let logs_dir_path = match env::var(LOGS_DIR_PATH_ENV_VAR) {
+/// Get the directory containing Nitro CLI related log files.
+pub fn get_log_file_base_path() -> String {
+    match env::var(LOGS_DIR_PATH_ENV_VAR) {
         Ok(env_path) => env_path,
         Err(_) => LOGS_DIR_PATH.to_string(),
-    };
-    Path::new(&logs_dir_path).join(LOG_FILE_NAME)
+    }
+}
+
+/// Get the path to the log file.
+fn get_log_file_path() -> PathBuf {
+    Path::new(&get_log_file_base_path()).join(LOG_FILE_NAME)
 }
 
 /// Open a file at a given location for writing and appending.
-fn open_log_file(file_path: &Path) -> Result<File> {
+fn open_log_file(file_path: &Path) -> NitroCliResult<File> {
     let file = OpenOptions::new()
         .create(true)
         .append(true)
         .read(false)
-        .open(file_path)?;
+        .open(file_path)
+        .map_err(|e| {
+            new_nitro_cli_failure!(
+                &format!("Failed to open log file: {:?}", e),
+                NitroCliErrorEnum::FileOperationFailure
+            )
+        })?;
+
+    let log_file_uid = Uid::from_raw(
+        file.metadata()
+            .map_err(|e| {
+                new_nitro_cli_failure!(
+                    &format!("Failed to get log file metadata: {:?}", e),
+                    NitroCliErrorEnum::FileOperationFailure
+                )
+            })?
+            .uid(),
+    );
 
     // The log file should be write-accessible to any user, since
     // any user may launch a CLI instance. Only the file's owner
     // may change its permissions.
-    if Uid::from_raw(file.metadata()?.uid()) == Uid::current() {
+    if log_file_uid == Uid::current() {
         let perms = Permissions::from_mode(0o766);
-        file.set_permissions(perms)?;
+        file.set_permissions(perms).map_err(|e| {
+            new_nitro_cli_failure!(
+                &format!("Failed to change log file permissions: {:?}", e),
+                NitroCliErrorEnum::FilePermissionsError
+            )
+        })?;
     }
 
     Ok(file)
 }
 
 /// Initialize logging.
-pub fn init_logger() -> EnclaveProcLogWriter {
+pub fn init_logger() -> NitroCliResult<EnclaveProcLogWriter> {
     // The log file is "nitro-cli.log" and is stored in the NPE resources directory.
-    let log_writer =
-        EnclaveProcLogWriter::new().ok_or_exit("Failed to initialize enclave process log writer.");
+    let log_writer = EnclaveProcLogWriter::new()?;
 
     // Initialize logging with the new log writer.
     flexi_logger::Logger::with_env_or_str(DEFAULT_LOG_LEVEL)
         .log_target(LogTarget::Writer(Box::new(log_writer.clone())))
         .start()
-        .ok_or_exit("Failed to initialize enclave process logger.");
+        .map_err(|e| {
+            new_nitro_cli_failure!(
+                &format!("Failed to initialize enclave process logger: {:?}", e),
+                NitroCliErrorEnum::LoggerError
+            )
+        })?;
 
     // The log writer is provided for sharing between CLI-related processes.
-    log_writer
+    Ok(log_writer)
 }
 
 #[cfg(test)]
@@ -243,12 +307,12 @@ mod tests {
         let _ = fs::create_dir(tmp_log_dir);
 
         let log_writer = EnclaveProcLogWriter::new().unwrap();
-        log_writer.update_logger_id("new-logger-id");
+        let _ = log_writer.update_logger_id("new-logger-id").unwrap();
         let lock_result = log_writer.logger_id.lock();
 
         assert!(lock_result.unwrap().eq("new-logger-id"));
 
-        log_writer.update_logger_id("");
+        let _ = log_writer.update_logger_id("").unwrap();
 
         if !path_existed {
             // Remove whole `tmp_log_dir` if necessary

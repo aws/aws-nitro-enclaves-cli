a6715f1a1f220e84f1af5f73ace54e8933ecd56d
Laurentiu Stefan <stefala@amazon.com>
2021-01-20 19:04:44 +0200

    build: Make the build on ARM functional
    This patch makes the nitro-cli build on ARM by specifying the
    `--sysroot` argument based on the architecture used in the
    bindings.
    
    This argument is needed for the compiler to locate the architecture
    depenendent header files needed by the `include/uapi/linux/nitro_enclaves.h`
    file.
    
    Signed-off-by: Laurentiu Stefan <stefala@amazon.com>

--- build.rs
+++ build.rs
@@ -1,10 +1,50 @@
 extern crate bindgen;
 
+use std::env;
+use std::io;
 use std::path::PathBuf;
 use std::process::Command;
 
 const HEADER_PATH: &str = "include/uapi/linux/nitro_enclaves.h";
 const OUT_FILE: &str = "/driver_structs.rs";
+
+// Generate the sysroot path based on the architecture the program is built.
+// This is needed for the compiler to locate the architecture dependent
+// header files.
+fn get_gcc_sysroot() -> String {
+    // Initialize the output because it might remain uninitialized with the
+    // current control flow
+    let mut output = Err(io::Error::from(io::ErrorKind::NotFound));
+
+    // Use the `CC` environment variable if existing
+    // which should give us directly the compiler's name
+    let cc = env::var("CC");
+    if let Ok(cc) = cc {
+        output = Command::new(cc).arg("--print-sysroot").output();
+    }
+
+    if let Err(_) = output {
+        // Otherwise, try to guess the compiler's name by the name
+        // $TARGET-gcc, where $TARGET represents the target architecture
+        // that was specified for build
+        let target = env::var("TARGET").unwrap();
+        output = Command::new(format!("{}-gcc", target))
+            .arg("--print-sysroot")
+            .output();
+        if let Err(_) = output {
+            // If that did not work as well, use directly `gcc`
+            output = Command::new("gcc").arg("--print-sysroot").output();
+        }
+    }
+
+    // At this point we should have a valid command output
+    // that gives us the sysroot path
+    let output = output.expect("no compiler was found for the current target");
+    let output = String::from_utf8_lossy(&output.stdout);
+    let path = output.trim().to_string();
+    println!("cargo:rerun-if-changed={}", path);
+    path
+}
 
 fn main() {
     // Get latest commit SHA.
@@ -30,6 +70,7 @@ fn main() {
     println!("cargo:rerun-if-changed={}", HEADER_PATH);
 
     let bindings = bindgen::Builder::default()
+        .clang_arg(format!("--sysroot={}", get_gcc_sysroot()))
         .header(HEADER_PATH)
         .whitelist_type("ne_.*")
         .whitelist_var("NE_ERR_.*")

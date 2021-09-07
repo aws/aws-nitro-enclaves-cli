0cfc75be21d70d9b78f2a03bb47b73de311fddbe
Alexandru Vasile <lexnv@amazon.com>
2021-09-07 18:16:11 +0300

    pylint: Lint the helpers module
    Apply python idiomatic concepts to increase the
    pylint of the module.
    
    pylint3 ./tests/integration/helpers.py
    [Before] Your code has been rated at -2.28/10
    [After]  Your code has been rated at 10.00/10
    
    Signed-off-by: Alexandru Vasile <lexnv@amazon.com>

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -1,160 +1,195 @@
 # Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
+#!/usr/bin/python3
+"""
+This module provides helpers for managing enclaves testing.
+"""
 
-import os
 import subprocess
 from subprocess import PIPE
 from subprocess import check_output
 import os
 import signal
 
-ROOT_DIR  = os.path.dirname(os.path.abspath(__file__)) + "/../../"
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) + "/../../"
 TEST_IMAGES = "test_images/"
 SAMPLE_EIF = "vsock-sample.eif"
 
-# Runs a system command and checks it returned success
+
 def run_cmd_ok(cmd_string):
-        print(cmd_string)
-        ret = os.system(cmd_string)
-        assert ret == 0
+    """Runs a system command and checks it returned success"""
+    print(cmd_string)
+    ret = os.system(cmd_string)
+    assert ret == 0
 
-# Runs a system command and checks it returned error
+
 def run_cmd_err(cmd_string):
-        print(cmd_string)
-        ret = os.system(cmd_string)
-        assert ret != 0
+    """Runs a system command and checks it returned error"""
+    print(cmd_string)
+    ret = os.system(cmd_string)
+    assert ret != 0
 
-# Checks dir_name and its children do not include any files
+
 def check_no_files(dir_name):
-        for root, dirs, files in os.walk(dir_name):
-                print("Expected files to be empty found: ", files)
-                assert len(files) == 0
+    """Checks dir_name and its children do not include any files"""
+    for _root, _dirs, files in os.walk(dir_name):
+        print("Expected files to be empty found: ", files)
+        assert len(files) == 0
 
-# Builds the prefix for running a make command
+
 def make_prefix(install_dir, build_dir):
-        return "make " + "NITRO_CLI_INSTALL_DIR=" + install_dir + \
-                        " OBJ_PATH=" + build_dir + \
-                        " -C " + ROOT_DIR + " "
+    """Builds the prefix for running a make command"""
+    return "make " + "NITRO_CLI_INSTALL_DIR=" + install_dir + \
+           " OBJ_PATH=" + build_dir + \
+           " -C " + ROOT_DIR + " "
 
-# Runs a process and dumps it outputs for debugging purpose
-# Returns an instance of a CompletedProcess
+
 def run_subprocess(args):
-        try:
-                result = subprocess.run(args, stdout=PIPE, stderr=PIPE)
-        except:
-                assert 0
-        output = result.stdout.decode('UTF-8');
-        error = result.stderr.decode('UTF-8');
-        print("Running command: ", args);
-        print("================= STDOUT =================")
-        print(output);
-        print("================= STDERR =================")
-        print(error);
+    """
+    Runs a process and dumps it outputs for debugging purpose
+    :return: An instance of a CompletedProcess
+    """
+    try:
+        result = subprocess.run(args, stdout=PIPE, stderr=PIPE, check=False)
+    except: # pylint: disable=bare-except
+        assert 0
+    output = result.stdout.decode('UTF-8')
+    error = result.stderr.decode('UTF-8')
+    print("Running command: ", args)
+    print("================= STDOUT =================")
+    print(output)
+    print("================= STDERR =================")
+    print(error)
 
-        # CLI should not panic in any circumstances
-        assert output.lower().find("panic") == -1
-        assert error.lower().find("panic") == -1
-        return result
+    # CLI should not panic in any circumstances
+    assert output.lower().find("panic") == -1
+    assert error.lower().find("panic") == -1
+    return result
+
 
 def get_pids(name):
-        try:
-                return check_output(["pidof",name]).decode('UTF-8')
-        except:
-                return ""
+    """Find the process ID of a running program"""
+    try:
+        return check_output(["pidof", name]).decode('UTF-8')
+    except: # pylint: disable=bare-except
+        return ""
+
 
 def kill_all_nitro_processes():
-        pids = get_pids("nitro-cli")
-        for pid in pids.split():
-                try:
-                        if pid:
-                                os.kill(int(pid), signal.SIGTERM)
-                except:
-                        print("Caught exception while killing the process");
+    """Kill al nitro-cli processes"""
+    pids = get_pids("nitro-cli")
+    for pid in pids.split():
+        try:
+            if pid:
+                os.kill(int(pid), signal.SIGTERM)
+        except: # pylint: disable=bare-except
+            print("Caught exception while killing the process")
 
-# Runs a process and checks it returned success
-# Returns an instance of a CompletedProcess
+
 def run_subprocess_ok(args):
-        result = run_subprocess(args)
-        assert result.returncode == 0
-        return result
+    """
+    Runs a process and checks it returned success
+    :return: An instance of a CompletedProcess
+    """
+    result = run_subprocess(args)
+    assert result.returncode == 0
+    return result
 
-# Runs a process and checks it returned error
-# Returns an instance of a CompletedProcess
+
 def run_subprocess_err(args):
-        result = run_subprocess(args)
-        assert result.returncode != 0
-        return result
-
-# Builds an run_enclave command
-def run_enclave_cmd(eif_name, memory, cpu_count, extra_flags = None):
-        eif_path = TEST_IMAGES + eif_name
-        args = [ "nitro-cli",
-                 "run-enclave",
-                 "--eif-path", eif_path,
-                 "--memory", memory,
-                 "--cpu-count", cpu_count
-                ]
-        if extra_flags is not None:
-                args.extend(extra_flags)
-        return args
-
-# Runs an enclave and checks it returned success
-# Returns an instance of a CompletedProcess
-def run_enclave_ok(eif_name, memory, cpu_count, extra_flags = None):
-        args = run_enclave_cmd(eif_name, memory, cpu_count, extra_flags)
-        return run_subprocess_ok(args)
-
-# Runs an enclave and checks it returned error
-# Returns an instance of a CompletedProcess
-def run_enclave_err(eif_name, memory, cpu_count, extra_flags = None):
-        args = run_enclave_cmd(eif_name, memory, cpu_count, extra_flags)
-        return run_subprocess_err(args)
+    """
+    Runs a process and checks it returned error
+    :return: An instance of a CompletedProcess
+    """
+    result = run_subprocess(args)
+    assert result.returncode != 0
+    return result
 
 
-# Terminates an enclave and checks the command was successful
-# Returns an instance of a CompletedProcess
+def run_enclave_cmd(eif_name, memory, cpu_count, extra_flags=None):
+    """Builds an run_enclave command"""
+    eif_path = TEST_IMAGES + eif_name
+    args = [
+        "nitro-cli", "run-enclave", "--eif-path", eif_path, "--memory", memory,
+        "--cpu-count", cpu_count
+    ]
+    if extra_flags is not None:
+        args.extend(extra_flags)
+    return args
+
+
+def run_enclave_ok(eif_name, memory, cpu_count, extra_flags=None):
+    """
+    Runs an enclave and checks it returned success
+    :return: An instance of a CompletedProcess
+    """
+    args = run_enclave_cmd(eif_name, memory, cpu_count, extra_flags)
+    return run_subprocess_ok(args)
+
+
+def run_enclave_err(eif_name, memory, cpu_count, extra_flags=None):
+    """
+    Runs an enclave and checks it returned error
+    :return: An instance of a CompletedProcess
+    """
+    args = run_enclave_cmd(eif_name, memory, cpu_count, extra_flags)
+    return run_subprocess_err(args)
+
+
 def terminate_enclave_ok(enclave_id):
-        args = [ "nitro-cli",
-                 "terminate-enclave",
-                 "--enclave-id", enclave_id
-        ]
+    """
+    Terminates an enclave and checks the command was successful
+    :return: An instance of a CompletedProcess
+    """
+    args = ["nitro-cli", "terminate-enclave", "--enclave-id", enclave_id]
 
-        return run_subprocess_ok(args)
+    return run_subprocess_ok(args)
 
-# Runs describe_enclaves command and checks the command was successful
-# Returns an instance of a CompletedProcess
+
 def describe_enclaves_ok():
-        args = [ "nitro-cli",
-                 "describe-enclaves"]
+    """
+    Runs describe_enclaves command and checks the command was successful
+    :return: An instance of a CompletedProcess
+    """
+    args = ["nitro-cli", "describe-enclaves"]
 
-        return run_subprocess_ok(args)
+    return run_subprocess_ok(args)
 
-# Runs describe_eif command, describing the EIF at the given path
-# Checks if command is successful and returns a CompletedProcess
+
 def describe_eif_ok(eif_name):
-        eif_path = TEST_IMAGES + eif_name
-        args = [ "nitro-cli",
-                 "describe-eif",
-                 "--eif-path", eif_path,
-                ]
-        return run_subprocess_ok(args)
+    """
+    Runs describe_eif command, describing the EIF at the given path
+    :return: Checks if command is successful and returns a CompletedProcess
+    """
+    eif_path = TEST_IMAGES + eif_name
+    args = [
+        "nitro-cli",
+        "describe-eif",
+        "--eif-path",
+        eif_path,
+    ]
+    return run_subprocess_ok(args)
 
-# Connects to the enclave console defined by the enclave id
-# Returns the handle to the running process.
-def connect_console(enclave_id, timeout = None):
-        args = ["nitro-cli",
-                "console",
-                "--enclave-id", enclave_id]
 
-        if timeout is not None:
-                args.extend(["--disconnect-timeout", str(timeout)])
+def connect_console(enclave_id, timeout=None):
+    """
+    Connects to the enclave console defined by the enclave id
+    :return: The handle to the running process
+    """
+    args = ["nitro-cli", "console", "--enclave-id", enclave_id]
 
-        return subprocess.Popen(args, stdout = PIPE, stderr = PIPE)
+    if timeout is not None:
+        args.extend(["--disconnect-timeout", str(timeout)])
 
-# Get the number of CPUs in the system, both on-line and off-line.
+    return subprocess.Popen(args, stdout=PIPE, stderr=PIPE)
+
+
 def get_cpu_count():
-        run_lscpu = subprocess.Popen(["lscpu -a -p=cpu"], stdout=subprocess.PIPE, shell=True)
-        (output, _) = run_lscpu.communicate()
-        output = output.decode('UTF-8').splitlines()
-        cpu_ids = [id for id in output if not id.startswith("#")]
-        return len(cpu_ids)
+    """Get the number of CPUs in the system, both on-line and off-line"""
+    try:
+        result = subprocess.run(["lscpu -a -p=cpu"], stdout=PIPE, check=True, shell=True)
+    except: # pylint: disable=bare-except
+        assert 0
+    output = result.stdout.decode('UTF-8').splitlines()
+    cpu_ids = [id for id in output if not id.startswith("#")]
+    return len(cpu_ids)

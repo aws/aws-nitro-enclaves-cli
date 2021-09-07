47b20601a10ef6fd87e6e66c94d4e5e7b2d1506b
Alexandru Vasile <lexnv@amazon.com>
2021-09-07 16:01:01 +0300

    pylint: Lint the test_installation module
    Apply python idiomatic concepts to increase the
    pylint of the module.
    
    pylint3 ./tests/integration/test_installation.py
    [Before] Your code has been rated at 0.00/10
    [After]  Your code has been rated at 10.00/10
    
    Signed-off-by: Alexandru Vasile <lexnv@amazon.com>

--- tests/integration/test_installation.py
+++ tests/integration/test_installation.py
@@ -1,45 +1,48 @@
 # Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 #!/usr/bin/python3
+"""
+This module tests the install/uninstall flow.
+"""
 
-import os
 import tempfile
 import helpers
 
-# Test for checking install/uninstall flows correctly
-# What it does:
-#    1. Runs make install in temp dir.
-#    2. Source the resulting env.sh.
-#    3. Check nitro-cli & vsock-proxy are in the path
-#       and ready to go
-#
-#
+
 def test_install_uninstall():
-        install_dir = tempfile.TemporaryDirectory()
-        build_dir = tempfile.TemporaryDirectory()
-        make = helpers.make_prefix(install_dir.name, build_dir.name)
+    """
+    Test for checking install/uninstall flows correctly
+    What it does:
+       1. Runs make install in temp dir.
+       2. Source the resulting env.sh.
+       3. Check nitro-cli & vsock-proxy are in the path and ready to go.
+    """
 
-        # Expect that the binaries are not present on the system
-        help_cmd = "nitro-cli --help || vsock-proxy --help"
-        helpers.run_cmd_err(help_cmd)
+    install_dir = tempfile.TemporaryDirectory()
+    build_dir = tempfile.TemporaryDirectory()
+    make = helpers.make_prefix(install_dir.name, build_dir.name)
 
-        install_cmd = make + " install"
-        build_nitro_cli = make + " nitro-cli"
-        build_vsock = make + " vsock-proxy"
-        helpers.run_cmd_ok(build_nitro_cli)
-        helpers.run_cmd_ok(build_vsock)
-        helpers.run_cmd_ok(install_cmd)
+    # Expect that the binaries are not present on the system
+    help_cmd = "nitro-cli --help || vsock-proxy --help"
+    helpers.run_cmd_err(help_cmd)
 
-        # Expect that we are able to successfully run the binaries.
-        help_cmd = "/bin/bash -ce \"source " + install_dir.name + \
-                       "/etc/profile.d//nitro-cli-env.sh && nitro-cli --help && vsock-proxy --help\""
-        helpers.run_cmd_ok(help_cmd)
+    install_cmd = make + " install"
+    build_nitro_cli = make + " nitro-cli"
+    build_vsock = make + " vsock-proxy"
+    helpers.run_cmd_ok(build_nitro_cli)
+    helpers.run_cmd_ok(build_vsock)
+    helpers.run_cmd_ok(install_cmd)
 
-        # Perform clean-up.
-        uninstall_cmd = make + " uninstall"
-        helpers.run_cmd_ok(uninstall_cmd)
-        helpers.check_no_files(install_dir.name)
+    # Expect that we are able to successfully run the binaries.
+    help_cmd = "/bin/bash -ce \"source " + install_dir.name + \
+                   "/etc/profile.d//nitro-cli-env.sh && nitro-cli --help && vsock-proxy --help\""
+    helpers.run_cmd_ok(help_cmd)
 
-        # Also remove the inserted driver.
-        rmmod_cmd = "rmmod nitro_enclaves"
-        helpers.run_cmd_ok(rmmod_cmd)
+    # Perform clean-up.
+    uninstall_cmd = make + " uninstall"
+    helpers.run_cmd_ok(uninstall_cmd)
+    helpers.check_no_files(install_dir.name)
+
+    # Also remove the inserted driver.
+    rmmod_cmd = "rmmod nitro_enclaves"
+    helpers.run_cmd_ok(rmmod_cmd)

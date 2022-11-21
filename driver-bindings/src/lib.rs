7539fc4ee48d62e36f52fbaa84bace1900cba1a0
Petre Eftime <epetre@amazon.com>
2022-11-21 09:32:58 +0000

    regenerate driver-bindings with Default
    Use latest version of bindgen (0.62) and add Default to implementations.
    This fixes `clippy::derivable_impls` warning and https://github.com/rust-lang/rust-bindgen/issues/1651.
    
    Signed-off-by: Petre Eftime <epetre@amazon.com>

--- driver-bindings/src/lib.rs
+++ driver-bindings/src/lib.rs
@@ -7,9 +7,5 @@
 #![allow(missing_docs)]
 #![allow(non_camel_case_types)]
 
-// Keep this until https://github.com/rust-lang/rust-bindgen/issues/1651 is fixed.
-#[cfg_attr(test, allow(deref_nullptr))]
 mod bindings;
 pub use self::bindings::*;
-mod wrappers;
-pub use self::wrappers::*;

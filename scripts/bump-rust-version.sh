f62b81ecc5678fdc7f558599bdbb55985f4861e6
Costin Lupu <lvpv@amazon.com>
2025-11-17 15:51:24 +0000

    scripts: Add bump-rust-version.sh for bumping MSRV
    We introduce the script that should help us bumping the Rust MSRV in all
    needed files.
    
    Signed-off-by: Costin Lupu <lvpv@amazon.com>

---
+++ scripts/bump-rust-version.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 <version>"
+	exit 2
+fi
+
+VERSION="$1"
+
+SCRIPT_DIR="$(realpath $(dirname $0))"
+
+pushd "$SCRIPT_DIR/.." >/dev/null
+
+# Update all Cargo.toml files
+while read -r toml_file; do
+	old_version=$(grep '^rust-version = ' $toml_file | tr -d ' "' | cut -d'=' -f2)
+	echo "Updating $toml_file from $old_version to $VERSION .."
+	sed -i "s/^rust-version = \"$old_version\"/rust-version = \"$VERSION\"/" "$toml_file"
+done < <(find -name "Cargo.toml" | sort)
+
+# Update .github/workflows/ci.yml
+sed -i "s/rust: \[[0-9]\+\.[0-9]\+\.[0-9]\+, stable, nightly\]/rust: [$VERSION, stable, nightly]/" .github/workflows/ci.yml
+
+# Update SOURCES/aws-nitro-enclaves-cli.spec
+sed -i "s/^BuildRequires: rust >= [0-9]\+\.[0-9]\+/BuildRequires: rust >= $VERSION/" SOURCES/aws-nitro-enclaves-cli.spec
+sed -i "s/^BuildRequires: cargo >= [0-9]\+\.[0-9]\+/BuildRequires: cargo >= $VERSION/" SOURCES/aws-nitro-enclaves-cli.spec
+
+# Update README.md
+sed -i "s|^\[msrv\]: https://img.shields.io/badge/MSRV-1.71.1-blue|[msrv]: https://img.shields.io/badge/MSRV-$VERSION-blue|" README.md
+
+# Update tools/Dockerfile
+sed -i "s/^ENV RUST_VERSION=[0-9]\+\.[0-9]\+\.[0-9]\+/ENV RUST_VERSION=$VERSION/" tools/Dockerfile
+
+popd >/dev/null
+

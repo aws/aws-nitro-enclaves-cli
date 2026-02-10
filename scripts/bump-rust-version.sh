3147c5712011f6aabc30d4b0937ca0bbf938fba1
Costin Lupu <lvpv@amazon.com>
2026-02-10 20:05:40 +0100

    scripts/bump-rust-version.sh: Switch to two-part versioning
    We already used two-part versioning when we bumped to 1.81, so we also
    need to update the script to handle such previous versions.
    
    Signed-off-by: Costin Lupu <lvpv@amazon.com>

--- scripts/bump-rust-version.sh
+++ scripts/bump-rust-version.sh
@@ -19,17 +19,17 @@ while read -r toml_file; do
 done < <(find -name "Cargo.toml" | sort)
 
 # Update .github/workflows/ci.yml
-sed -i "s/rust: \[[0-9]\+\.[0-9]\+\.[0-9]\+, stable, nightly\]/rust: [$VERSION, stable, nightly]/" .github/workflows/ci.yml
+sed -i "s/rust: \[[0-9]\+\.[0-9]\+, stable, nightly\]/rust: [$VERSION, stable, nightly]/" .github/workflows/ci.yml
 
 # Update SOURCES/aws-nitro-enclaves-cli.spec
 sed -i "s/^BuildRequires: rust >= [0-9]\+\.[0-9]\+/BuildRequires: rust >= $VERSION/" SOURCES/aws-nitro-enclaves-cli.spec
 sed -i "s/^BuildRequires: cargo >= [0-9]\+\.[0-9]\+/BuildRequires: cargo >= $VERSION/" SOURCES/aws-nitro-enclaves-cli.spec
 
 # Update README.md
-sed -i "s|^\[msrv\]: https://img.shields.io/badge/MSRV-1.71.1-blue|[msrv]: https://img.shields.io/badge/MSRV-$VERSION-blue|" README.md
+sed -i "s|^\[msrv\]: https://img.shields.io/badge/MSRV-[0-9]\+\.[0-9]\+-blue|[msrv]: https://img.shields.io/badge/MSRV-$VERSION-blue|" README.md
 
 # Update tools/Dockerfile
-sed -i "s/^ENV RUST_VERSION=[0-9]\+\.[0-9]\+\.[0-9]\+/ENV RUST_VERSION=$VERSION/" tools/Dockerfile
+sed -i "s/^ENV RUST_VERSION=[0-9]\+\.[0-9]\+/ENV RUST_VERSION=$VERSION/" tools/Dockerfile
 
 popd >/dev/null
 
